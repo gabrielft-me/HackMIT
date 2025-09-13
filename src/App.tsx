a36d3736720cf419c756074b0e57e6b2c8e514b4
Connor Lai <connorlai@hotmail.com>
2025-09-13 17:54:50 -0400

    input box skeleton

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [inputText, setInputText] = useState('')
+
+  const handleSubmit = () => {
+    console.log(inputText)
+  }
+
+  return (
+    <div className="container">
+      <div className="image-placeholder"></div>
+      <div className="input-container">
+        <input
+          id="test-input"
+          type="text"
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+          placeholder="test"
+        />
+        <button onClick={handleSubmit}>Enter</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
