51945a622e3d0b7e1477712412f2efe401e61c74
Connor Lai <connorlai@hotmail.com>
2025-09-14 03:14:50 -0400

    updated search box width

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { Logo } from "../components/Logo";
 import { InputBox } from "../components/InputBox";
 
 const Home: React.FC = () => {
     return (
     <>
-      <Logo />
       <InputBox />
     </>
   )
