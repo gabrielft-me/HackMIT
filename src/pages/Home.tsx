80270825a275a9f15c122a85ce6fb85c1e56ebbf
michael-fedorov <michael@mike-home.net>
2025-09-14 03:43:41 -0400

    updated home logo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,20 @@ import { Logo } from "../components/Logo";
 import { InputBox } from "../components/InputBox";
 
 const Home: React.FC = () => {
-    return (
-    <>
-      <Logo />
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        marginTop: "48px",
+      }}
+    >
+      <div style={{ marginBottom: "24px" }}>
+        <Logo width={350} height={300} />
+      </div>
       <InputBox />
-    </>
-  )
-}
+    </div>
+  );
+};
 export default Home;
